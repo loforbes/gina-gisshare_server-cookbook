e301f460b47a25a6034b5027e82ae64ff6e4bd68
Liam Forbes <Loforbes@alaska.edu>
2015-01-26 13:03:46 -0900

    initial cookbook generation

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'gina-gisshare_server'
+maintainer       'UAF'
+maintainer_email 'support+chef@gina.alaska.edu'
+license          'apache2'
+description      'Installs/Configures gina-gisshare_server'
+long_description 'Installs/Configures gina-gisshare_server'
+version          '0.1.0'
+
